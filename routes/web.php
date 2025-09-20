20086ea9c87ba5191b2a1d279b528c6935f17195
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-09-21 00:00:14 +0700

    feat: adding route for api (authentication) and web (home)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    
+    // === BERANDA & PRODUK (Guest/Public Routes) ===
+    Route::get('/beranda', [HomeController::class, 'beranda'])->name('api.beranda');
+    Route::get('/categories', [HomeController::class, 'getCategories'])->name('api.categories');
+    Route::get('/categories/{categoryId}/products', [HomeController::class, 'getProductsByCategory'])->name('api.category.products');
+    Route::get('/products/{productId}', [HomeController::class, 'getProduct'])->name('api.product.detail');
+    Route::get('/search', [HomeController::class, 'searchProducts'])->name('api.search');
+    
 });
