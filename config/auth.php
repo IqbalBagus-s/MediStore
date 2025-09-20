7e2f859f985829ab4965430604521c6e231f34d8
iqbal.bs_ <119472842+IqbalBagus-s@users.noreply.github.com>
2025-09-20 23:53:55 +0700

    feat: Adding the Sanctum file and making changes to authentication for Sanctum.

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'sanctum', // Pastikan ini sanctum
+            'provider' => 'users',
+        ],
     ],
 
     /*
